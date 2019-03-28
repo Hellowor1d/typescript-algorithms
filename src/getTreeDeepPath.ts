1898db48aa13f65901550dc35a7da14fa0c3c1d4
hellowor1d <1034597776@qq.com>
2019-03-29 00:57:46 +0800

    todo

--- src/getTreeDeepPath.ts
+++ src/getTreeDeepPath.ts
@@ -2,7 +2,7 @@ interface TreeNode {
     value : number
     children?: TreeNode[];
 }
-
+//TODO·
 function getTreeDeepPath(tree:TreeNode[],parent:TreeNode[]){
     for(let i=0, len=tree.length; i<len; i++){
         if(tree[i].children){
