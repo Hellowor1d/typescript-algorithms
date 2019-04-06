d824e33e80d71a027eaf866b9d9c3bc5d83c31fc
Hellowor1d <1034597776@qq.com>
2019-04-07 00:57:04 +0800

    ts enum 枚举类型使用很方便

--- dist/ts-practice/基础类型.js
+++ dist/ts-practice/基础类型.js
@@ -16,4 +16,17 @@ let mixTypeArr;
 mixTypeArr = ['string', 123, true, [1, 2]];
 //TODO 这里和文档介绍有出入，越界元素并未使用联合类型，而是产生报错
 // mixTypeArr[4] = 1 //报错 Type '1' is not assignable to type 'undefined'
+//枚举类型 默认情况下，从0开始为元素编号
+//很方便可以用来做类型-编码对应表
+var Role;
+(function (Role) {
+    Role[Role["teacher"] = 0] = "teacher";
+    Role[Role["student"] = 1] = "student";
+    Role[Role["monitor"] = 4] = "monitor";
+})(Role || (Role = {}));
+let man1 = Role[1];
+let man3 = Role[4];
+console.log(Role); //{0: "teacher", 1: "student", 4: "monitor", teacher: 0, student: 1, monitor: 4}
+console.log(man1); // student
+console.log(man3); // monitor
 //# sourceMappingURL=基础类型.js.map
--- src/ts-practice/基础类型.ts
+++ src/ts-practice/基础类型.ts
@@ -20,3 +20,12 @@ let mixTypeArr:[string, number, boolean, number[]]
 mixTypeArr = ['string', 123, true, [1,2]]
 //TODO 这里和文档介绍有出入，越界元素并未使用联合类型，而是产生报错
 // mixTypeArr[4] = 1 //报错 Type '1' is not assignable to type 'undefined'
+
+//枚举类型 默认情况下，从0开始为元素编号
+//很方便可以用来做类型-编码对应表
+enum Role {teacher, student, monitor=4}
+let man1 = Role[1]
+let man3 = Role[4]
+console.log(Role); //{0: "teacher", 1: "student", 4: "monitor", teacher: 0, student: 1, monitor: 4}
+console.log(man1); // student
+console.log(man3); // monitor
